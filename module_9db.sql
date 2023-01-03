9d23ef89a9e75685440338025942dfc64354e4fc
v-barber <113566313+v-barber@users.noreply.github.com>
2023-01-02 18:37:41 -0600

    Updates on challenge

---
+++ module_9db.sql
@@ -0,0 +1,69 @@
+--List the employee number, last name, first name, sex, and salary of each employee
+SELECT employees.emp_no,
+employees.last_name,
+employees.first_name,
+employees.sex,
+salaries.salary
+FROM employees
+JOIN Salaries
+ON employees.emp_no = salaries.emp_no;
+--List the first name, last name, and hire date for the employees who were hired in 1986
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date 1986;
+--List the manager of each department along with their department number, department name, employee number, last name, and first name
+SELECT departments.dept_no, 
+departments.dept_name,
+dept_manager.emp_no,
+employees.last_name,
+employees.first_name
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name
+SELECT dept_emp.emp_no,
+employees.last_name,
+employees.first_name,
+departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no=departments.dept_no;
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+--List each employee in the Sales department, including their employee number, last name, and first name
+SELECT dept_emp.emp_no, 
+employees.last_name,
+employees.first_name,
+departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name 
+SELECT dept_emp.emp_no,
+employees.last_name,
+employees.first_name,
+departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments.dept_name = 'Sales' AND OR 'Development';
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name)
+SELECT last_name,
+COUNT() 
+GROUP BY last_name
+ORDER BY
+COUNT() DESC;
+
+
+
+
+alter table departments.csv rename to departments;
---
+++ schemas.sql
@@ -0,0 +1,62 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/gn5TOt
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+CREATE TABLE "departments" (
+    "dept_no" text NOT NULL,
+    "dept_name" varchar NOT NULL,
+    PRIMARY KEY dept_no
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int NOT NULL,
+    "dept_no" text NOT NULL,
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" text NOT NULL,
+    "emp_no" int NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int NOT NULL,
+    "emp_title" text NOT NULL,
+    "birth_date" int NOT NULL,
+    "first_name" varchar NOT NULL,
+    "last_name" varchar NOT NULL,
+    "sex" varchar NOT NULL,
+    "hire_date" int NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int NOT NULL,
+    "salary" int NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" text NOT NULL,
+    "title" varchar NOT NULL,
+    PRIMARY KEY(title_id)
+);
+
+ALTER TABLE
+    "departments" FOREIGN KEY("dept_no") REFERENCES "dept_emp" ("dept_no");
+
+ALTER TABLE
+    "dept_emp" FOREIGN KEY("emp_no") REFERENCES "salaries" ("emp_no");
+
+ALTER TABLE
+    "dept_manager" FOREIGN KEY("dept_no") REFERENCES "departments" ("dept_no");
+
+ALTER TABLE
+    "dept_manager" FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no");
+
+ALTER TABLE
+    "employees" FOREIGN KEY("emp_title") REFERENCES "titles" ("title");
+
+ALTER TABLE
+    "salaries" FOREIGN KEY("emp_no") REFERENCES "dept_manager" ("emp_no");
